897ff7bf3497e0a8f7a3cbe6477375c477d4770d
adithak <adithak59@gmail.com>
2017-06-28 14:38:24 +0530

    Update possitive.c

--- possitive.c
+++ possitive.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  clrscr();
+  printf("enter your name");
+  scanf("%d",&n);
+  if(n>0)
+    printf("the number is possitive %d",n);
+  else if(n==0)
+    printf("the number is zero %d",n);
+  else
+    printf("the number is negative %d",n);
+  getch();
+}
